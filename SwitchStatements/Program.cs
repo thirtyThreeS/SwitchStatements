47ed7be3037944d0371c4ca80fa9cf38f171e6dd
thirtyThreeS <thaumasios@outlook.com>
2021-11-26 14:51:01 -0500

    initial commit

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SwitchStatements
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite subject in school? Note: Make your first letter capital followed by lowercase letters while spelling out your favorite subject!");
+            string subject = Console.ReadLine();
+
+            switch (subject)
+            {
+                case "Math":
+                    Console.WriteLine("Math! letters and numbers and paradigms.. oh my..");
+                    break;
+                case "English":
+                    Console.WriteLine("English! I ponder how we can fail our own language..");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science! the study of how things work in this world.. very interesting indeed");
+                    break;
+                case "Art":
+                    Console.WriteLine("Art! May I see some of your artwork?");
+                    break;
+                case "Gym":
+                    Console.WriteLine("Gym! Do you even lift?");
+                    break;
+                default:
+                    Console.WriteLine("Oh, very nice!");
+                    break;
+                                          
+            }
+        }
+    }
+}
